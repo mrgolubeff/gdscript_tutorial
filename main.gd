db86157b377d6e153eb5f28500dc135d03ba7d68
Vladimir Golubev <mrgolubeff@icloud.com>
2024-05-13 11:32:46 +0300

    Print "Hello, World!" on the screen

--- main.gd
+++ main.gd
@@ -3,4 +3,5 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hello, mrgolubeff!")
+	$Label.text = "Hello, mrgolubeff!"
+	$Label.modulate = Color.PURPLE
